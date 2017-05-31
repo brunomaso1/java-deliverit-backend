2fb251df5a2716527e1096daafd5f089d5757fca
brunomaso1 <brunomaso1@hotmail.com>
2017-05-31 16:06:43 -0300

    Pa devolver la ubicacion
    Pa devolver la ubicacion

--- src/main/java/ucu/deliverit/backcore/entidades/servicios/DeliveryFacadeREST.java
+++ src/main/java/ucu/deliverit/backcore/entidades/servicios/DeliveryFacadeREST.java
@@ -15,103 +15,117 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import ucu.deliverit.backcore.entidades.Delivery;
+import ucu.deliverit.backcore.entidades.Ubicacion;
 import ucu.deliverit.backcore.respuestas.RespuestaGeneral;
 
 @Stateless
 @Path("delivery")
 public class DeliveryFacadeREST extends AbstractFacade<Delivery> {
 
-    @PersistenceContext(unitName = "ucu.deliverit_BackCore_war_1.0PU")
-    private EntityManager em;
+	@PersistenceContext(unitName = "ucu.deliverit_BackCore_war_1.0PU")
+	private EntityManager em;
 
-    public DeliveryFacadeREST() {
-        super(Delivery.class);
-    }
+	public DeliveryFacadeREST() {
+		super(Delivery.class);
+	}
 
-    @POST
-    @Override
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public RespuestaGeneral create(Delivery entity) {
-        RespuestaGeneral r = new RespuestaGeneral();
-        
-        if (entity == null) {
-            r.setCodigo(RespuestaGeneral.CODIGO_ERROR_VALOR_NULO);
-            r.setMensaje("Delivery" + RespuestaGeneral.MENSAJE_VALOR_NULO);
-            r.setObjeto(null);
-        } else {
-            r = super.create(entity);
-        }
-        return r;
-    }
+	@POST
+	@Override
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public RespuestaGeneral create(Delivery entity) {
+		RespuestaGeneral r = new RespuestaGeneral();
 
-    @PUT
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void edit(@PathParam("id") Integer id, Delivery entity) {
-        super.edit(entity);
-    }
+		if (entity == null) {
+			r.setCodigo(RespuestaGeneral.CODIGO_ERROR_VALOR_NULO);
+			r.setMensaje("Delivery" + RespuestaGeneral.MENSAJE_VALOR_NULO);
+			r.setObjeto(null);
+		} else {
+			r = super.create(entity);
+		}
+		return r;
+	}
 
-    @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Integer id) {
-        super.remove(super.find(id));
-    }
+	@PUT
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void edit(@PathParam("id") Integer id, Delivery entity) {
+		super.edit(entity);
+	}
 
-    @GET
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Delivery find(@PathParam("id") Integer id) {
-        return super.find(id);
-    }
+	@DELETE
+	@Path("{id}")
+	public void remove(@PathParam("id") Integer id) {
+		super.remove(super.find(id));
+	}
 
-    @GET
-    @Override
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Delivery> findAll() {
-        return super.findAll();
-    }
-    
-    @GET
-    @Path("findBySucursal/{idSucursal}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Delivery> findBySucursal(@PathParam("idSucursal") Integer idSucursal) {
-        String consulta = "SELECT DISTINCT d FROM Delivery d"
-                + " JOIN d.viajeCollection v"
-                + " WHERE v.sucursal.id = :idSucursal";     
-        List<Delivery> results = em.createNamedQuery("Delivery.findBySucursal")
-            .setParameter("idSucursal", idSucursal)
-            .getResultList();
-        return results;
-    }
-    
-    @GET
-    @Path("findAllSinViajesEnProceso")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Delivery> findAllSinViajesEnProceso() {
-        List<Delivery> results = em.createNamedQuery("Delivery.findBySucursal")
-            .getResultList();
-        
-        return results;
-    }
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Delivery find(@PathParam("id") Integer id) {
+		return super.find(id);
+	}
 
-    @GET
-    @Path("{from}/{to}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Delivery> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
-    }
+	@GET
+	@Override
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Delivery> findAll() {
+		return super.findAll();
+	}
 
-    @GET
-    @Path("count")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String countREST() {
-        return String.valueOf(super.count());
-    }
+	@GET
+	@Path("findBySucursal/{idSucursal}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Delivery> findBySucursal(@PathParam("idSucursal") Integer idSucursal) {
+		String consulta = "SELECT DISTINCT d FROM Delivery d"
+				+ " JOIN d.viajeCollection v"
+				+ " WHERE v.sucursal.id = :idSucursal";
+		List<Delivery> results = em.createQuery(consulta, Delivery.class)
+				.setParameter("idSucursal", idSucursal)
+				.getResultList();
+		return results;
+	}
 
-    @Override
-    protected EntityManager getEntityManager() {
-        return em;
-    }
-    
+	@GET
+	@Path("findAllSinViajesEnProceso")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Delivery> findAllSinViajesEnProceso() {
+		List<Delivery> results = em.createNamedQuery("Delivery.findBySucursal")
+				.getResultList();
+
+		return results;
+	}
+	
+	@GET
+	@Path("getUbicacion/{idDelivery}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Ubicacion getUbicacion(@PathParam("idDelivery") Integer idSucursal) {
+		String consulta = "SELECT u FROM Ubicacion u "
+				+ " JOIN u.delivery d"
+				+ " WHERE d.id = :idDelivery";
+		Ubicacion result = em.createQuery(consulta, Ubicacion.class)
+				.setParameter("idDelivery", idSucursal)
+				.getSingleResult();
+		return result;
+	}
+
+	@GET
+	@Path("{from}/{to}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Delivery> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+		return super.findRange(new int[]{from, to});
+	}
+
+	@GET
+	@Path("count")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String countREST() {
+		return String.valueOf(super.count());
+	}
+
+	@Override
+	protected EntityManager getEntityManager() {
+		return em;
+	}
+
 }
